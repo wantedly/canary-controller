670cb618a5536f0d115b09d63e38002c61316e74
Yuichi Saito <munisystem@gmail.com>
2018-12-19 17:14:54 +0900

    kubebuilder create api --group canary --version v1beta1 --kind Canary

---
+++ pkg/apis/canary/v1beta1/canary_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2018 Wantedly, Inc..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CanarySpec defines the desired state of Canary
+type CanarySpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// CanaryStatus defines the observed state of Canary
+type CanaryStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Canary is the Schema for the canaries API
+// +k8s:openapi-gen=true
+type Canary struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CanarySpec   `json:"spec,omitempty"`
+	Status CanaryStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CanaryList contains a list of Canary
+type CanaryList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Canary `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Canary{}, &CanaryList{})
+}
