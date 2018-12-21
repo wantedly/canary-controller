a7ed60d236464e0b06169af5120e3355dd6e7c58
Yuichi Saito <munisystem@gmail.com>
2018-12-21 20:23:32 +0900

    Fix test

--- pkg/controller/canary/canary_controller_test.go
+++ pkg/controller/canary/canary_controller_test.go
@@ -34,29 +34,31 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-var c client.Client
-
-var target = &appsv1.Deployment{
-	ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default"},
-	Spec: appsv1.DeploymentSpec{
-		Selector: &metav1.LabelSelector{
-			MatchLabels: map[string]string{"deployment": "foo-deployment"},
-		},
-		Template: corev1.PodTemplateSpec{
-			ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": "foo-deployment"}},
-			Spec: corev1.PodSpec{
-				Containers: []corev1.Container{
-					{
-						Name:  "nginx",
-						Image: "nginx",
+var (
+	c      client.Client
+	target = &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{Name: "foo", Namespace: "default", Labels: map[string]string{"app": "nginx"}},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"deployment": "foo-deployment"},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": "foo-deployment"}},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "nginx",
+							Image: "nginx:latest",
+						},
 					},
 				},
 			},
 		},
-	},
-}
-var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo-canary", Namespace: "default"}}
-var depKey = types.NamespacedName{Name: "foo-canary", Namespace: "default"}
+	}
+	targetKey       = types.NamespacedName{Name: "foo", Namespace: "default"}
+	expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo-canary", Namespace: "default"}}
+	depKey          = types.NamespacedName{Name: "foo-canary", Namespace: "default"}
+)
 
 const timeout = time.Second * 5
 
@@ -93,6 +95,8 @@ func TestReconcile(t *testing.T) {
 	}
 	g.Expect(err).NotTo(gomega.HaveOccurred())
 	defer c.Delete(context.TODO(), target)
+	g.Eventually(func() error { return c.Get(context.TODO(), targetKey, &appsv1.Deployment{}) }, timeout).
+		Should(gomega.Succeed())
 
 	// Create the Canary object and expect the Reconcile and Deployment to be created
 	err = c.Create(context.TODO(), instance)
@@ -115,8 +119,4 @@ func TestReconcile(t *testing.T) {
 	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
 	g.Eventually(func() error { return c.Get(context.TODO(), depKey, deploy) }, timeout).
 		Should(gomega.Succeed())
-
-	// Manually delete Deployment since GC isn't enabled in the test control plane
-	g.Expect(c.Delete(context.TODO(), deploy)).To(gomega.Succeed())
-
 }
